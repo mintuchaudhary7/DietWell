33d06a5a39fc8c8986af89ebca4cf6923de7530f
mintu kumar <mintuchaudharychd45b@gmail.com>
2024-03-06 12:15:17 +0530

    testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/Navbar';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Homepage";
 import Login from "./components/Login";
@@ -10,18 +10,18 @@ import Contact from "./components/Contact";
 import Signup from "./components/Signup";
 function App() {
   return (
-    <div className = "h-full " >
+    <div className="h-full ">
       <Navbar></Navbar>
       <Routes>
-                <Route path="/" element = {<Home></Home>}/>
-                <Route path="/services" element = {<Services></Services>}/>
-                <Route path="about" element = {<About></About>}/>
-                <Route path="/contact" element = {<Contact></Contact>}/>
-                <Route path="/login" element = {<Login></Login>}/>
-                <Route path="/signup" element = {<Signup></Signup>}/>
-          </Routes>
+        <Route path="/" element={<Home></Home>} />
+        <Route path="/services" element={<Services></Services>} />
+        <Route path="about" element={<About></About>} />
+        <Route path="/contact" element={<Contact></Contact>} />
+        <Route path="/login" element={<Login></Login>} />
+        <Route path="/signup" element={<Signup></Signup>} />
+      </Routes>
     </div>
-  )
+  );
 }
 
 export default App;
--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-const Signup = ()=>{
-    const [Name, setName] = useState("");
+const Signup = () => {
+  const [Name, setName] = useState("");
   const [Email, setEmail] = useState("");
   const [ContactNo, setContactno] = useState(0);
-  const [Passward,setPassward] = useState("");
+  const [Passward, setPassward] = useState("");
   const [error, setError] = useState("");
 
   const navigate = useNavigate();
-    const handleSubmit = async(e)=>{
-        e.preventDefault();
-    var addUser = { Name, Email, ContactNo,Passward };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    var addUser = { Name, Email, ContactNo, Passward };
     console.log(addUser);
 
     const response = await fetch("http://localhost:2000/signup", {
@@ -31,42 +31,51 @@ const Signup = ()=>{
       console.log(result);
       setName("");
       setEmail("");
-    //   setAge(0);
+      //   setAge(0);
       setError("");
       setContactno("");
       setPassward("");
       navigate("/login");
     }
-    }
-    return (
-        <div>
-            <div>Welcome to Signup page</div>
-            <form  onSubmit={handleSubmit}>
-                
-                <label htmlFor="name">Name</label>
-                <input id="name" type="text" value={Name}
-            onChange={(e) => setName(e.target.value)} />
+  };
+  return (
+    <div>
+      <div>Welcome to Signup page</div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          id="name"
+          type="text"
+          value={Name}
+          onChange={(e) => setName(e.target.value)}
+        />
 
-                <label htmlFor="gmail">gmail</label>
-                <input id="gmail" type="email" value={Email}
-            onChange={(e) => setEmail(e.target.value)} />
+        <label htmlFor="gmail">gmail</label>
+        <input
+          id="gmail"
+          type="email"
+          value={Email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-                <label htmlFor="passward">passward</label>
-                <input id="paswward" type="text" value={Passward}
-            onChange={(e) => setPassward(e.target.value)} />
+        <label htmlFor="passward">passward</label>
+        <input
+          id="paswward"
+          type="text"
+          value={Passward}
+          onChange={(e) => setPassward(e.target.value)}
+        />
 
-                <label htmlFor="contact">contact no.</label>
-                <input id="contact" type="number" value={ContactNo}
-            onChange={(e) => setContactno(e.target.value)} />
-                <button type="submit">submit</button>
-
-            </form>
-           
-
-        </div>
-        
-        
-        
-    )
-}
+        <label htmlFor="contact">contact no.</label>
+        <input
+          id="contact"
+          type="number"
+          value={ContactNo}
+          onChange={(e) => setContactno(e.target.value)}
+        />
+        <button type="submit">submit</button>
+      </form>
+    </div>
+  );
+};
 export default Signup;
