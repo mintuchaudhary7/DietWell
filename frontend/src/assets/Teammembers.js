1fc27af1148abcf8ae9a54b5f6d98f24f388b3a7
mintu kumar <mintuchaudharychd45b@gmail.com>
2024-03-13 11:31:02 +0530

    about us page updated

--- frontend/src/assets/Teammembers.js
+++ frontend/src/assets/Teammembers.js
@@ -1,6 +1,6 @@
 import sahil from './4957136.jpg'
 const teamMembers = [
-    { name: 'Deepanshu Singh', role: 'Developer', imageUrl: sahil },
+    { name: 'Deepanshu Singh', role: 'Lead Developer', imageUrl: sahil },
     { name: 'Mintu Chaoudhry', role: 'CEO, Marketing Head', imageUrl: sahil },
     { name: 'Kanishka', role: 'Lead Designer', imageUrl: sahil },
     { name: 'Ayan Firdosh', role: 'Project Manager', imageUrl: sahil },
--- frontend/src/components/Forrgottenpassward.js
+++ frontend/src/components/Forrgottenpassward.js
@@ -6,19 +6,19 @@ import { useContext } from "react";
 const Forrgottenpassward = () => {
   const user = useContext(UserContext);
   const changePasswardOtpVerified = user.changePasswardOtpVerified;
-  const setchangePasswardOtpVerified = user.setchangePasswardOtpVerified
-  const navigate = useNavigate()
-  const[Email,setEmail] = useState("");
-  const[otp,setOtp] = useState("");
-  const[emailsent,setEmailSend] = useState(false);
-  const[otpbackend,setotpbackend] = useState("");
-  const[error,setError] = useState(false);
-  const[errormsg,setErrorMsg] = useState("");
-  
+  const setchangePasswardOtpVerified = user.setchangePasswardOtpVerified;
+  const navigate = useNavigate();
+  const [Email, setEmail] = useState("");
+  const [otp, setOtp] = useState("");
+  const [emailsent, setEmailSend] = useState(false);
+  const [otpbackend, setotpbackend] = useState("");
+  const [error, setError] = useState(false);
+  const [errormsg, setErrorMsg] = useState("");
+
   const EmailSubmit = async (e) => {
     e.preventDefault();
-    
-    var data = {Email};
+
+    var data = { Email };
     console.log(data);
     const response = await fetch("http://localhost:2000/forgottenpassward", {
       method: "POST",
@@ -27,90 +27,82 @@ const Forrgottenpassward = () => {
       },
       body: JSON.stringify(data),
     });
-  
-    const result = await response.json();
-    
-   
-    if (!response.ok) {
-     
-      // setEmailSend(false);
-      setErrorMsg(result.message)
-      setError(true);
-      console.log(result)
 
+    const result = await response.json();
+
+    if (!response.ok) {
+      // setEmailSend(false);
+      setErrorMsg(result.message);
+      setError(true);
+      console.log(result);
     }
     if (response.ok) {
       console.log(result);
-      setEmailSend(true)
-      setError(!error)
-      setotpbackend(result.message)
-      console.log("otp is"+result.message)
+      setEmailSend(true);
+      setError(!error);
+      setotpbackend(result.message);
+      console.log("otp is" + result.message);
     }
   };
   const otpSubmit = async (e) => {
     e.preventDefault();
-    if(otpbackend === otp){
+    if (otpbackend === otp) {
       setchangePasswardOtpVerified(true);
-      if(changePasswardOtpVerified){
+      if (changePasswardOtpVerified) {
         navigate("/changepassward");
       }
-      
-    }
-    else{
+    } else {
       setError(true);
-      setErrorMsg("Wrong otp please enter again")
+      setErrorMsg("Wrong otp please enter again");
     }
   };
   return (
     <div>
-      {error ? <h1 className="text-red-600">{errormsg}</h1> :<></>}
+      {error ? <h1 className="text-red-600">{errormsg}</h1> : <></>}
 
-      {emailsent? <form action="" onSubmit={otpSubmit}>
-       <label
-      className="block text-gray-700 text-sm font-bold mb-2"
-      htmlFor="otp"
-    >
-      otp
-    </label>
-    <input
-      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-      id="otp"
-      type="number"
-      placeholder="123456"
-      name="otp"
-      value={otp}
-      onChange={(e) => setOtp(e.target.value)}
-      required
-    />
-   
-      <button type="submit">Submit</button>
+      {emailsent ? (
+        <form action="" onSubmit={otpSubmit}>
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="otp"
+          >
+            otp
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="otp"
+            type="number"
+            placeholder="123456"
+            name="otp"
+            value={otp}
+            onChange={(e) => setOtp(e.target.value)}
+            required
+          />
 
-      </form> :<form action="" onSubmit={EmailSubmit}>
-      
-    <label
-        className="block text-gray-700 text-sm font-bold mb-2"
-        htmlFor="email"
-      >
-        Email
-      </label>
-      <input
-        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        id="email"
-        type="email"
-        placeholder="Email"
-        name="email"
-        value={Email}
-        onChange={(e) => setEmail(e.target.value)}
-        required
-      />
-      <button type="submit">Submit</button>
-
-      </form>   }
-      
-     
-        
+          <button type="submit">Submit</button>
+        </form>
+      ) : (
+        <form action="" onSubmit={EmailSubmit}>
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="email"
+          >
+            Email
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="email"
+            type="email"
+            placeholder="Email"
+            name="email"
+            value={Email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <button type="submit">Submit</button>
+        </form>
+      )}
     </div>
-    
-  )
+  );
 };
 export default Forrgottenpassward;
