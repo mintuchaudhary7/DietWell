2a0e4fdf0c58adb0b46b875ad70d4a323d033e00
Deepanshu44v <deepanshu2070.be21@chitkara.edu.in>
2024-05-09 21:43:27 +0530

    error in protected route and middleware is fixed initially we are only checking token is present in users req or not we are not verifing the user token weather it is expired or not now it is fixed

--- backend/middleware/authentication.js
+++ backend/middleware/authentication.js
@@ -61,11 +61,11 @@ const user = require('../models/schema');
 
 const auth = async (req, res, next) => {
   try {
-    // const token = req.cookies.token;
-    const token = req.headers.authorization.split(' ')[1] 
+    //const token = req.cookies.token;
+    const token = req.headers.authorization.split(" ")[1];
     console.log(req.cookies);
 
-    if (!token || token===undefined || token==="null") {
+    if (!token || token===undefined || token === "null") {
       return res.status(400).json({
         success: false,
         message: "Please login",
